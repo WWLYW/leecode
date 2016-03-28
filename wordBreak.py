4342397314491ef9965ef2a079e0806410ac23f8
yaowei <yaowei@bj-m-6849a.local>
2016-03-28 18:35:43 +0800

    test

---
+++ wordBreak.py
@@ -0,0 +1,34 @@
+"""
+Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
+
+Return true because "leetcode" can be segmented as "leet code".
+
+Subscribe to see which companies asked this question
+"""
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: Set[str]
+        :rtype: bool
+        """
+        if len(s) == 0 or len(wordDict) == 0:
+            return False
+
+        findwordList = [0]
+        for i in xrange(1, len(s) + 1):
+            for j in xrange(len(findwordList) - 1, -1, -1):
+                substr = s[findwordList[j]:i]
+                if substr in wordDict:
+                    findwordList.append(i)
+                    break
+        return findwordList[-1] == len(s) 
+
+a = Solution()
+s = "leetcode"
+wordDict = ['l', 'le', 'lee', 'tcode']
+print a.wordBreak(s, wordDict)
